b34ec1218c4aac89e91ca657872a853697d9af0c
bwz3rbot <68409068+bwz3rbot@users.noreply.github.com>
2022-08-21 08:57:55 -0700

    Add login page and install firebase lib

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Shop from "./Shop";
+import Login from "./Login";
 import Navigation from "./Navigation";
 export default function Router() {
 	return (
@@ -9,6 +10,7 @@ export default function Router() {
 				<Route path="" element={<Navigation />}>
 					<Route index element={<Home />} />
 					<Route path="shop" element={<Shop />} />
+					<Route path="login" element={<Login />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
